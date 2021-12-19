9991ea464d907de344b4e1514e2efd948af1b46c
Ethan <ethan@plexiate.com>
2021-12-19 18:40:57 -0500

    testing

--- src/main/java/com/ehhthan/scholarapi/ScholarAPI.java
+++ src/main/java/com/ehhthan/scholarapi/ScholarAPI.java
@@ -7,10 +7,13 @@ import com.ehhthan.scholarapi.resourcepack.ResourcePack;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Singleton;
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
 
-@Singleton
 public class ScholarAPI {
     private static ScholarAPI INSTANCE;
     private final ResourcePack resourcePack;
@@ -21,8 +24,11 @@ public class ScholarAPI {
         this.resourcePack = injector.getInstance(ResourcePack.class);
     }
 
-    public static ScholarAPI getInstance(File resourcesDirectory) {
-        return (INSTANCE != null) ? INSTANCE : new ScholarAPI(resourcesDirectory);
+    public static ScholarAPI getInstance(File resourcesDirectory) throws ZipException {
+        ZipFile file = new ZipFile(resourcesDirectory);
+        String path = resourcesDirectory.getPath() + "ScholarAPI";
+        file.extractAll(path);
+        return (INSTANCE != null) ? INSTANCE : new ScholarAPI(new File(path, "assets"));
     }
 
     public ResourcePack getPack() {
