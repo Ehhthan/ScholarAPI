d275e198f479fef1f2499ef54b4ba41274d83b30
Ethan <ethan@plexiate.com>
2021-12-19 20:38:30 -0500

    instance

--- src/main/java/com/ehhthan/scholarapi/ScholarAPI.java
+++ src/main/java/com/ehhthan/scholarapi/ScholarAPI.java
@@ -1,18 +1,11 @@
 package com.ehhthan.scholarapi;
 
-import com.ehhthan.scholarapi.asset.font.FontAsset;
-import com.ehhthan.scholarapi.asset.font.character.FontCharacter;
-import com.ehhthan.scholarapi.namespacedkey.NamespacedKeyFactory;
 import com.ehhthan.scholarapi.resourcepack.ResourcePack;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.Singleton;
-import net.lingala.zip4j.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
 
 public class ScholarAPI {
     private static ScholarAPI INSTANCE;
@@ -25,10 +18,9 @@ public class ScholarAPI {
     }
 
     public static ScholarAPI getInstance(File resourcesDirectory) throws ZipException {
-        ZipFile file = new ZipFile(resourcesDirectory);
-        String path = resourcesDirectory.getPath() + "ScholarAPI";
-        file.extractAll(path);
-        return (INSTANCE != null) ? INSTANCE : new ScholarAPI(new File(path, "assets"));
+        if (INSTANCE == null)
+            INSTANCE = new ScholarAPI(resourcesDirectory);
+        return INSTANCE;
     }
 
     public ResourcePack getPack() {
